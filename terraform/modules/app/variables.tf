3116639f3954bc30c39d854e12ee3fdfa7a88574
naniko23 <96186396+naniko23@users.noreply.github.com>
2022-05-15 03:41:06 +0300

    Update variables.tf

--- terraform/modules/app/variables.tf
+++ terraform/modules/app/variables.tf
@@ -1,14 +1,10 @@
 variable public_key_path {
-  description = "Path to the public key used for ssh access"
+description = "Path to the public key used for ssh access"
 }
-variable image_id {
-  description = "Disk image for reddit app"
-  default = "fd8emqvr1350ndn2r6dv"
+variable app_disk_image {
+description = "Disk image for reddit app"
+default = "reddit-app-base"
 }
 variable subnet_id {
-  description = "Subnets for modules"
-}
-variable app_zone {
-  description = "Application zone"
-  default = "ru-central1-a"
+description = "Subnets for modules"
 }
